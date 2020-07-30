2ddea72fbb48fa572e7b9f443088e2e1a7935f1b
Obaid Shahzad <ObaidShahzad1@Gmail.com>
2020-07-30 18:14:08 +0500

    Add files via upload
    Added logger.py module

--- consumer.py
+++ consumer.py
@@ -1,35 +1,18 @@
+#!/usr/bin/env python
 import pika
-import mysql.connector
-import datetime
-import logger
 
-credentials = pika.PlainCredentials('admin', 'root')
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+
 channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
-    global cursor
-    global sqlconnection
-    body=body.decode("utf-8")
-    data=eval(body)
-    logger.receivedData(data)
-    process_id=data["process_id"]
-    process_id=int(process_id)+1
-    time_now=str(datetime.datetime.now())
-    time_now=time_now[:19]
-    query=f"UPDATE customers SET `process_id`='{process_id}' , `process_time`= '{time_now}' where id={data['id']}"
-    cursor.execute(query)
-    sqlconnection.commit()
-#------------- Function Definition Ended -------------------------
-try:
-    sqlconnection = mysql.connector.connect(host='10.0.1.98',database='campaign',user='root',password='awais')
-    cursor = sqlconnection.cursor()
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-    logger.msg()
-    channel.start_consuming()
-except mysql.connector.Error as error:
-    logger.tableUpdateFailed(error)
-finally:
-    if (sqlconnection.is_connected()):
-        connection.close()
+    print(" [x] Received %r" % body)
+
+
+channel.basic_consume(
+    queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()

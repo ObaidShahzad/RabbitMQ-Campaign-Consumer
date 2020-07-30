c9d9b502c63953790dd7c2a691abdc550000e8f9
Obaid Shahzad <ObaidShahzad1@Gmail.com>
2020-07-30 06:32:45 -0700

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,7 +1,7 @@
 import pika
 import mysql.connector
 import datetime
-import logger
+from logger import generateLogs
 
 credentials = pika.PlainCredentials('admin', 'root')
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
@@ -13,7 +13,7 @@ def callback(ch, method, properties, body):
     global sqlconnection
     body=body.decode("utf-8")
     data=eval(body)
-    logger.receivedData(data)
+    generateLogs.info(f" [x] Received {data}")
     process_id=data["process_id"]
     process_id=int(process_id)+1
     time_now=str(datetime.datetime.now())
@@ -21,15 +21,15 @@ def callback(ch, method, properties, body):
     query=f"UPDATE customers SET `process_id`='{process_id}' , `process_time`= '{time_now}' where id={data['id']}"
     cursor.execute(query)
     sqlconnection.commit()
-#------------- Function Definition Ended -------------------------
+
 try:
     sqlconnection = mysql.connector.connect(host='10.0.1.98',database='campaign',user='root',password='awais')
     cursor = sqlconnection.cursor()
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-    logger.msg()
+    generateLogs.info(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 except mysql.connector.Error as error:
-    logger.tableUpdateFailed(error)
+    generateLogs.error("Failed to update table record: {}".format(error))
 finally:
     if (sqlconnection.is_connected()):
         connection.close()

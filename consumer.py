223e3a8cab0c7bf81483f1cfdb279401ea49d992
Obaid Shahzad <ObaidShahzad1@Gmail.com>
2020-07-30 06:11:42 -0700

    Update consumer.py
    Added the logger.py module

--- consumer.py
+++ consumer.py
@@ -1,16 +1,19 @@
 import pika
 import mysql.connector
 import datetime
+import logger
+
 credentials = pika.PlainCredentials('admin', 'root')
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.121',5672,'/',credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
+
 def callback(ch, method, properties, body):
     global cursor
     global sqlconnection
     body=body.decode("utf-8")
     data=eval(body)
-    print(f" [x] Received {data}")
+    logger.receivedData(data)
     process_id=data["process_id"]
     process_id=int(process_id)+1
     time_now=str(datetime.datetime.now())
@@ -18,14 +21,15 @@ def callback(ch, method, properties, body):
     query=f"UPDATE customers SET `process_id`='{process_id}' , `process_time`= '{time_now}' where id={data['id']}"
     cursor.execute(query)
     sqlconnection.commit()
+#------------- Function Definition Ended -------------------------
 try:
     sqlconnection = mysql.connector.connect(host='10.0.1.98',database='campaign',user='root',password='awais')
     cursor = sqlconnection.cursor()
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    logger.msg()
     channel.start_consuming()
 except mysql.connector.Error as error:
-    print("Failed to update table record: {}".format(error))
+    logger.tableUpdateFailed(error)
 finally:
     if (sqlconnection.is_connected()):
         connection.close()
